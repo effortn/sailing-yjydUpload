82b0658f3ed007e2b2e26bd1ec52c8f03985077b
effortn <effortn@outlook.com>
2018-12-10 11:07:55 +0800

    项目初始化

---
+++ src/main/java/com/sailing/yjydupload/YjydUploadApplication.java
@@ -0,0 +1,12 @@
+package com.sailing.yjydupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YjydUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YjydUploadApplication.class, args);
+    }
+}
